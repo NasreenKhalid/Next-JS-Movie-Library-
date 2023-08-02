695e3d139e38c82c91644392d5a1167540d7266a
Nasreen Shehzad <nasreen.1@live.com>
2023-08-02 17:46:19 +0300

    homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,22 @@
 export default function HomePage(){
-    return <p>Coming Soon: a movie library</p>
+    return (
+<main>
+    <h1>My Movie Library</h1>
+    <p>Here is a list of popular movies</p>
+
+    <ul>
+    <li>Indiana Jones and the Dial of Destiny</li>
+        <li>Guardians of the Galaxy Vol. 3</li>
+        <li>Mission: Impossible - Dead Reckoning Part One</li>
+        <li>Spider-Man: Across the Spider-Verse</li>
+        <li>The Super Mario Bros. Movie</li>
+    </ul>
+<p>
+<a href="https://www.themoviedb.org/">See more</a>
+</p>
+
+</main>
+
+
+    )
 }
